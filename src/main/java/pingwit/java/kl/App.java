413e9d9658f5cb5d607de1eb26f4ac68159378a2
AndTab <pozand47@gmail.com>
2022-07-20 21:17:42 +0200

    first files

---
+++ src/main/java/pingwit/java/kl/App.java
@@ -0,0 +1,13 @@
+package pingwit.java.kl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
