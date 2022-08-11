e6081458875bcf8a7e87e00fa52849946e69ae29
And <pozand47@gmail.com>
2022-08-11 18:08:56 +0200

    hw20

---
+++ src/main/java/pingwit/java/kl/hw20/task_1/Company.java
@@ -0,0 +1,4 @@
+package pingwit.java.kl.hw20.task_1;
+
+public record Company(Long id, String ticker, String company_name, Long profit_year, String location, Long update_date_time) {
+}
---
+++ src/main/java/pingwit/java/kl/hw20/task_1/JdbcCompany.java
@@ -0,0 +1,10 @@
+package pingwit.java.kl.hw20.task_1;
+
+public class JdbcCompany {
+    private static final String URL ="jdbc:postgresql://localhost:5432/first_dat";
+    private static final String USER ="postgres";
+    private static final String PASSWORD = "docker";
+    
+    
+    
+}
