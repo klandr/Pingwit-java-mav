30e10a07cdd17b1c57678ab3a220b23d17838f53
And <pozand47@gmail.com>
2022-08-16 19:52:22 +0200

    hw20

--- src/main/java/pingwit/java/kl/hw20/task_1/JdbcCompany.java
+++ src/main/java/pingwit/java/kl/hw20/task_1/JdbcCompany.java
@@ -1,35 +1,29 @@
 package pingwit.java.kl.hw20.task_1;
 
-
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import org.postgresql.Driver;
 
 public class JdbcCompany {
-    private static final String URL ="jdbc:postgresql://localhost:5432/first_dat";
-    private static final String USER ="postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_dat";
+    private static final String USER = "postgres";
     private static final String PASSWORD = "docker";
 
-        public void runQuery() throws ClassNotFoundException, SQLException {
+    public void runQuery() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
-        try (Connection connection = DriverManager.getConnection(URL,USER,PASSWORD)){
-           Statement statement = connection.createStatement();
-           ResultSet rs = statement.executeQuery("SELECT * FROM actions");
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM actions");
 
-           List <Company> companies = new ArrayList<>();
+            List<Company> companies = new ArrayList<>();
 
-           while (rs.next()){
-               Company company = new Company(rs.getLong(1),rs.getString(2),
-                   rs.getString(3),rs.getLong(4),
-                   rs.getNString(5),rs.getLong(6) );
-               companies.add(company);
-           }
+            while (rs.next()) {
+                Company company = new Company(rs.getLong(1), rs.getString(2),
+                        rs.getString(3), rs.getLong(4),
+                        rs.getString(5), rs.getString(6));
+                companies.add(company);
+            }
             System.out.println(companies);
         }
-
-
     }
-
 }
